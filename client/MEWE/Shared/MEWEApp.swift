11104679be733fb1429de636b09ac5866b8221a6
dev-Lena <keunnalee@gmail.com>
2021-01-19 17:37:18 +0900

    chore: 프로젝트 생성

---
+++ client/MEWE/Shared/MEWEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEWEApp.swift
+//  Shared
+//
+//  Created by Keunna Lee on 2021/01/19.
+//
+
+import SwiftUI
+
+@main
+struct MEWEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
