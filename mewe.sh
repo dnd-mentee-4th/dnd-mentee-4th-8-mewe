6c466fcedab33482242ee8befe4393f7bbf46177
myeongkil <clevermk7211@gmail.com>
2021-01-30 15:44:24 +0900

    modified to sh

--- mewe.sh
+++ mewe.sh
@@ -1,41 +1,34 @@
+#!/bin/sh
+
 # The build tool has not been decided yet, and initially proceeds to the this shell.
 rootDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 databaseDir=$rootDir/database
 
-function usage
-{
+usage () {
 	echo "usage template"
 }
 
-function build
-{
+build () {
 	docker network inspect mewe >/dev/null 2>&1 || \
 	    docker network create mewe
 }
 
-function database
-{
+database () {
 	cd $databaseDir && docker-compose up -d
 }
 
-
-
-function down
-{
+down () {
 	cd $databaseDir && docker-compose down -v
 }
 
-function clean
-{
+clean () {
         down
         docker network rm mewe > /dev/null 2>&1
 	rm -rf $databaseDir/user_data
 	rm -rf $databaseDir/mewe_data
-
 }
 
-function mewe
-{
+mewe () {
 	case $1 in
 		build | up | frontend | backend | database | down | clean )
 			cmd=$1
