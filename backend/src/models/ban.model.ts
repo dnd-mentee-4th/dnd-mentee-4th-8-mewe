fe83ee1b2bda45824d1b008d5e8817108e04ee5b
myeongkil <clevermk7211@gmail.com>
2021-01-30 15:53:58 +0900

    init/getUser tested

--- backend/src/models/ban.model.ts
+++ backend/src/models/ban.model.ts
@@ -1,7 +1,10 @@
-import { Entity, Column } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class BanModel {
+    @PrimaryGeneratedColumn()
+    idx: number;
+
     @Column()
     email: string;
 
