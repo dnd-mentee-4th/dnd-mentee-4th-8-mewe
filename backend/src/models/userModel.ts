fe1b4b68d6724f2c0a401970e4f729090b758e23
myeongkil <clevermk7211@gmail.com>
2021-01-30 15:44:24 +0900

    feature: added user model

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,3 +1,70 @@
-export class userModel {
-    
+import { logger } from "../utils/logger";
+import { UserType } from "./types/user";
+
+export class UserModel {
+    private user: UserType;
+
+    constructor(user: UserType) {
+        this.user = user;
+        if (!this.user.friends) {
+            this.user.friends = [];
+        }
+        if (!this.user.banList) {
+            this.user.banList = [];
+        }
+    }
+
+    public get(): UserType {
+        return this.user;
+    }
+
+    public getEmail(): string {
+        return this.user.email;
+    }
+
+    public getName(): string {
+        return this.user.nickname;
+    }
+
+    public isBlock(): boolean {
+        return this.user.block;
+    }
+
+    public getFriends(): string[] {
+        return this.user.friends;
+    }
+
+    public getBanList(): string[] {
+        return this.user.banList;
+    }
+
+    public setName(nickname: string) {
+        this.user.nickname = nickname;
+    }
+
+    public addFriend(friendEmail: string) {
+        try {
+            this.user.friends.push(friendEmail);
+        } catch (error) {
+            logger.error('user.addFriend(friendEmail: string)');
+            throw new Error(error);
+        }
+    }
+
+    public addUserToBanList(userEmail: string) {
+        try {
+            this.user.banList.push(userEmail);
+        } catch (error) {
+            logger.error('user.addUserToBanList(userEmail: string)');
+            throw new Error(error);
+        }
+    }
+
+    public block() {
+        this.user.block = true;
+    }
+
+    public unblock() {
+        this.user.block = false;
+    }
 }
