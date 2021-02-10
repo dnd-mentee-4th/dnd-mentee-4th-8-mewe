5f65a43177a921dc17a47c47bdd466f8956f0a15
dev-Lena <keunnalee@gmail.com>
2021-02-10 14:01:02 +0900

    faet: implement Monthly Representative Emoji View

--- client/MEWE/MEWE/MonthlyChart/View/MonthlyChartView.swift
+++ client/MEWE/MEWE/MonthlyChart/View/MonthlyChartView.swift
@@ -70,7 +70,7 @@ struct MonthlyChartView: View {
                         }
                     }
                     
-                    
+                    MonthlyRepresentativeEmojiView()
                     Spacer()
                     Spacer()
                 }
