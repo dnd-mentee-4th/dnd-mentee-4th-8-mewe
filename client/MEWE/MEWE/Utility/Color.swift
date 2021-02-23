c08012b5b753b6e8b3b23b4fe400097c21ca342e
dev-Lena <keunnalee@gmail.com>
2021-02-23 23:38:43 +0900

    chore: add asset colors in Color extension

---
+++ client/MEWE/MEWE/Utility/Color.swift
@@ -0,0 +1,28 @@
+//
+//  Color.swift
+//  MEWE
+//
+//  Created by Keunna Lee on 2021/02/23.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let pinkishGrey = Color("pinkishGrey")
+    static let softBlue = Color("softBlue") // Key Color
+    static let softBlueDarker = Color("softBlueDarker")
+    static let veryLightBlue = Color("veryLightBlue")
+    
+    // MARK: - Emoji colors
+    static let angerColor = Color("angerColor")
+    static let concernColor = Color("concernColor")
+    static let easeColor = Color("easeColor")
+    static let flustrationColor = Color("flustrationColor")
+    static let happinessColor = Color("happinessColor")
+    static let heartColor = Color("heartColor")
+    static let joyColor = Color("joyColor")
+    static let proudColor = Color("proudColor")
+    static let sadnessColor = Color("sadnessColor")
+    static let tirednessColor = Color("tirednessColor")
+}
