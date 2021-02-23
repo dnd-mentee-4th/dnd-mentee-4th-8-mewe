d3c35a9f8e6a41f1ee2b790ef3e1f1089ca1155a
dev-Lena <keunnalee@gmail.com>
2021-02-23 23:39:01 +0900

    chore: add asset images in Image extension

---
+++ client/MEWE/MEWE/Utility/Image.swift
@@ -0,0 +1,23 @@
+//
+//  Image.swift
+//  MEWE
+//
+//  Created by Keunna Lee on 2021/02/23.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    // MARK: - Emoji Images
+    static let emoji_anger = Image("emoji_anger")
+    static let emoji_concern = Image("emoji_concern")
+    static let emoji_ease = Image("emoji_ease")
+    static let emoji_flustration = Image("emoji_flustration")
+    static let emoji_happiness = Image("emoji_happiness")
+    static let emoji_heart = Image("emoji_heart")
+    static let emoji_joy = Image("emoji_joy")
+    static let emoji_proud = Image("emoji_proud")
+    static let emoji_sadness = Image("emoji_sadness")
+    static let emoji_tiredness = Image("emoji_tiredness")
+}
