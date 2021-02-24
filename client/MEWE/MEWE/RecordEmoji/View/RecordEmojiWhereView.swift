3b49af848ddbb432798eb24919b8917d7187e737
madilyn <lollin0@naver.com>
2021-02-24 12:56:46 +0900

    RecordEmojiTextView에서 네비게이션 바 버튼 동작

--- client/MEWE/MEWE/RecordEmoji/View/RecordEmojiWhereView.swift
+++ client/MEWE/MEWE/RecordEmoji/View/RecordEmojiWhereView.swift
@@ -12,39 +12,39 @@ struct RecordEmojiWhereView: View {
     @Environment(\.presentationMode) private var presentationMode
     
     var body: some View {
-            VStack {
-                Text("오늘 하루를 어디에서 보냈나요?")
-                    .font(.title)
-                Text("감정 기록 잠금 해제 후, 감정 지도에서\n사람들에게 공유하고 공감을 얻을 수 있어요!")
-                    .font(.subheadline)
-                    .multilineTextAlignment(.center)
-                Button(action: {}, label: {
-                    NavigationLink(
-                        destination: EmptyView()){
-                        Text("감정 지도 바로가기")
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 8.0)
-                                    .stroke(lineWidth: 2.0)
-                                    .foregroundColor(.blue)
-                                    
-                            ).padding()
-                    }
-                })
-                .padding()
-                
-                Spacer(minLength: 0)
-            }
+        VStack {
+            Text("오늘 하루를 어디에서 보냈나요?")
+                .font(.title)
+            Text("감정 기록 잠금 해제 후, 감정 지도에서\n사람들에게 공유하고 공감을 얻을 수 있어요!")
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+            Button(action: {}, label: {
+                NavigationLink(
+                    destination: EmptyView()){
+                    Text("감정 지도 바로가기")
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8.0)
+                                .stroke(lineWidth: 2.0)
+                                .foregroundColor(.blue)
+                            
+                        ).padding()
+                }
+            })
+            .padding()
             
-            .navigationBarItems(
-                leading: LeadingNavView() {
-                    DispatchQueue.main.async {
-                        presentationMode.wrappedValue.dismiss()
-                    }
-                },
-                trailing: TrailingNavViewForWhere()
-            )
+            Spacer(minLength: 0)
+        }
+        
+        .navigationBarItems(
+            leading: LeadingNavView() {
+                DispatchQueue.main.async {
+                    presentationMode.wrappedValue.dismiss()
+                }
+            },
+            trailing: TrailingNavViewForWhere()
+        )
         .navigationBarBackButtonHidden(true)
-
+        
         .padding()
     }
 }
