666819f8b1032ba8ee51057d84a13d9355bbe091
madilyn <lollin0@naver.com>
2021-02-24 18:47:48 +0900

    add EmojiCalendarView

--- client/MEWE/MEWE/Calendar/View/CalendarView.swift
+++ client/MEWE/MEWE/Calendar/View/CalendarView.swift
@@ -6,11 +6,68 @@
 //
 
 import SwiftUI
+import FSCalendar
+
+class EmojiCalendar: UIViewController, FSCalendarDelegate{
+    var calendar = FSCalendar()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendar.delegate = self
+        calendar.appearance.titleDefaultColor = .black
+        calendar.appearance.titleWeekendColor = .black
+        calendar.appearance.headerTitleColor = .black
+        calendar.appearance.weekdayTextColor = .black
+        calendar.appearance.caseOptions = FSCalendarCaseOptions.weekdayUsesSingleUpperCase
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
+        calendar.backgroundColor = UIColor.white.withAlphaComponent(0)
+//        calendar.appearance.eventDefaultColor = .gray
+//        calendar.appearance.eventSelectionColor = .gray
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        calendar.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: view.frame.size.width)
+        view.addSubview(calendar)
+    }
+}
+
+extension EmojiCalendar {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        calendar.scope = .week
+    }
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        calendar.scope = .month
+    }
+}
 
 struct CalendarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+
+            EmojiCalendarController()
+            Button(action: {
+
+            }, label: {
+                Text("close")
+            })
+
+        }
+
+
     }
+}
+
+struct EmojiCalendarController: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<EmojiCalendarController>) -> EmojiCalendar {
+        return EmojiCalendar()
+    }
+
+    func updateUIViewController(_ uiViewController: EmojiCalendar, context: UIViewControllerRepresentableContext<EmojiCalendarController>) {
+    }
+
 }
 
 struct CalendarView_Previews: PreviewProvider {
