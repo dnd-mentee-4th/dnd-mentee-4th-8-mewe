0e7068658b78df6d83003e0c4911c4d75b5a15ea
dev-Lena <keunnalee@gmail.com>
2021-02-25 22:39:24 +0900

    refactor: modify RoundedBottomNavigationView

--- client/MEWE/MEWE/Common/RoundedBottomNavigationView.swift
+++ client/MEWE/MEWE/Common/RoundedBottomNavigationView.swift
@@ -19,14 +19,16 @@ struct RoundedBottomNavigationView<Content: View, Destination : View>: View {
     @State var active = false
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
-    init(destination: Destination,
-         isRoot: Bool,
-         isLast: Bool,
-         color: Color = .black,
-         height: CGFloat = 100,
-         backButtonImageName: String = SystemImageName.arrowLeft,
-         nextButtonImageName: String = SystemImageName.arrowRight,
-         @ViewBuilder content: () -> Content) {
+    init(
+        destination: Destination,
+        isRoot: Bool,
+        isLast: Bool,
+        color: Color = .black,
+        height: CGFloat = 100,
+        backButtonImageName: String = SystemImageName.arrowLeft,
+        nextButtonImageName: String = SystemImageName.arrowRight,
+        @ViewBuilder content: () -> Content
+    ) {
         self.destination = destination
         self.isRoot = isRoot
         self.isLast = isLast
@@ -38,60 +40,58 @@ struct RoundedBottomNavigationView<Content: View, Destination : View>: View {
     }
     
     var body: some View {
-        NavigationView {
-            GeometryReader { geometry in
-                Color.white
-                VStack {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25, style: .continuous)
-                                        .fill(Color.white)
-                            .frame(width: geometry.size.width, height: height)
-                            .mask(RoundedRectangle(cornerRadius: 25, style: .continuous))
-                            .shadow(color: Color(UIColor.lightGray), radius: 5, x: 0, y: 5)
-                                .edgesIgnoringSafeArea(.top)
-                        Spacer()
-                        HStack {
-                            // Back Button
-                            Image(systemName: backButtonImageName)
-                                    .frame(width: 20)
-                                    .foregroundColor(color)
-                                .onTapGesture(count: 1, perform: {
-                                    self.mode.wrappedValue.dismiss()
-                                }).opacity(isRoot ? 0 : 1)
-                            Spacer()
-                            
-                            // Title
-                            self.content
-                            
-                            // Next Button
-                            Spacer()
-                            Image(systemName: nextButtonImageName)
-                                .frame(width: 20)
-                                .foregroundColor(color)
-                                .onTapGesture(count: 1, perform: {
-                                    self.active.toggle()
-                                })
-                                .opacity(isLast ? 0 : 1)
-                            
-                            NavigationLink(
-                                destination: destination.navigationBarHidden(true)
-                                    .navigationBarHidden(true),
-                                isActive: self.$active,
-                                label: {
-                                    //no label
-                                })
-                        }
-                        .padding([.leading,.trailing], 15)
-                        .padding(.top, 50)
-                        .frame(width: geometry.size.width)
-                        .font(.system(size: 22))
-
-                    }
-                    .frame(width: geometry.size.width, height: 90)
-                    .edgesIgnoringSafeArea(.top)
+        GeometryReader { geometry in
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(Color.white)
+                    .frame(width: geometry.size.width, height: height)
+                    .mask(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .shadow(color: Color(UIColor.lightGray), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                HStack {
+                    // Back Button
+                    Image(systemName: backButtonImageName)
+                        .frame(width: 20)
+                        .foregroundColor(color)
+                        .onTapGesture(count: 1, perform: {
+                            self.mode.wrappedValue.dismiss()
+                        }).opacity(isRoot ? 0 : 1)
+                    Spacer()
+                    
+                    // Title
+                    self.content
+                    
+                    // Next Button
+                    Spacer()
+                    Image(systemName: nextButtonImageName)
+                        .frame(width: 20)
+                        .foregroundColor(color)
+                        .onTapGesture(count: 1, perform: {
+                            self.active.toggle()
+                        })
+                        .opacity(isLast ? 0 : 1)
+                    
+                    NavigationLink(
+                        destination: destination.navigationBarHidden(true)
+                            .navigationBarHidden(true),
+                        isActive: self.$active,
+                        label: {
+                            //no label
+                        })
                 }
-            }.navigationBarHidden(true)
-        }
+                .padding([.leading,.trailing], 15)
+                .padding(.top, 50)
+                .frame(width: geometry.size.width)
+                .font(.system(size: 22))
+                
+            } //: Z
+            .frame(width: geometry.size.width)
+        } //: G
+        .navigationBarHidden(true)
+        
+        .frame(height: 90)
     }
 }
 
